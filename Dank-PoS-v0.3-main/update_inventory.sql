1bad2e86fa63d3652253986a73d631574409eee0
RaoG812 <140965516+RaoG812@users.noreply.github.com>
2025-08-07 19:39:46 +0700

    Delete Dank-PoS-v0.3-main directory

--- Dank-PoS-v0.3-main/update_inventory.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE public.inventory ADD COLUMN IF NOT EXISTS barcode_id text;
-ALTER TABLE public.inventory ALTER COLUMN barcode_id SET DEFAULT substr(md5(random()::text), 1, 8);
-UPDATE public.inventory
-SET barcode_id = substr(md5(random()::text), 1, 8)
-WHERE barcode_id IS NULL;
-CREATE UNIQUE INDEX IF NOT EXISTS idx_inventory_barcode_id ON public.inventory (barcode_id);
